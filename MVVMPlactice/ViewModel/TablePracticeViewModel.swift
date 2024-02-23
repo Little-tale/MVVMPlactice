ee9e7c2991f678dfb28390b917a8b64ec710efcf
judge_happily <you3192001@gmail.com>
2024-02-24 01:00:04 +0900

    간단히 테이블뷰로 연습한 MVVM

--- MVVMPlactice/ViewModel/TablePracticeViewModel.swift
+++ MVVMPlactice/ViewModel/TablePracticeViewModel.swift
@@ -13,12 +13,9 @@ class TablePracticeViewModel{
     var userObserver: Observer<[User]> = Observer([])
 
     init(){
-        userObserver.value = [
-            User(name: "연습", age: 24),
-            User(name: "하자", age: 41),
-            User(name: "항상", age: 28),
-            User(name: "잊어도", age: 12)
-        ]
+        userObserver.bind { user in
+            self.addUserRandom()
+        }
     }
     
     func resetButtonAction() {
@@ -32,6 +29,12 @@ class TablePracticeViewModel{
     
     func removeLast(){
         print(#function)
+        guard let data = userObserver.value else {
+            return
+        }
+        if data.isEmpty {
+            return
+        }
         userObserver.value?.removeLast()
     }
     
